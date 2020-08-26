6e00c9d058f60bcfd50ed9a62fd06365a172419c
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2020-08-26 17:39:25 -0400

    added files because idk what im doing

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
+bot.login(config.token)
+
+bot.on("ready", () => {
+    console.log(bot.user.tag)
+})
+
+bot.on("messageReactionAdd", (reaction, user) => {
+    if (user.bot) return;
+    if (reaction.count == 1) {
+        const embed = {
+            "title": "Member has added a reaction",
+            "description": `${user} added the ${reaction.emoji.name} reaction to this [message](${reaction.message.url}).`,
+            "color": 9442302,
+            "timestamp": Date.now(),
+            "footer": {
+                "text": `ID: ${user.id}`
+            },
+            "thumbnail": {
+                "url": reaction.emoji.url
+            },
+            "author": {
+                "name": user.tag,
+                "icon_url": user.avatarURL()
+            }
+        };
+
+        bot.channels.cache.get(config.channelid).send({ embed })
+
+    }
+})
