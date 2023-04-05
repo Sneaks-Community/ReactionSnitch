469f0eb1f20cfd094dff95674584c5feabe4d00b
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2023-04-05 15:20:42 -0400

    AI Refactor and djs and version bump

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const {Client, Intents} = require("discord.js");
-const bot = new Client({intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
+const { Client, Intents } = require("discord.js");
 const config = require("./config.json");
-bot.login(config.token)
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
+bot.login(config.token);
 
 bot.on("ready", () => {
-    console.log(bot.user.tag)
-    bot.user.setActivity(require("./package.json").version)
-})
+    console.log(bot.user.tag);
+    bot.user.setActivity(require("./package.json").version);
+});
 
 bot.on("messageReactionAdd", (reaction, user) => {
-    if (user.bot) return;
-    if (reaction.count == 1) {
-        const embed = {
-            "title": "Member has added a reaction",
-            "description": `${user} added the ${reaction.emoji.name} reaction to this [message](${reaction.message.url}) in ${reaction.message.channel}.`,
-            "color": 9442302,
-            "timestamp": Date.now(),
-            "footer": {
-                "text": `ID: ${user.id}`
-            },
-            "thumbnail": {
-                "url": reaction.emoji.url
-            },
-            "author": {
-                "name": user.tag,
-                "icon_url": user.avatarURL()
-            }
-        };
+    if (user.bot || reaction.count !== 1) return;
 
-        bot.channels.cache.get(config.channelid).send({ embeds: [embed] })
+    const embed = {
+        "title": "Member has added a reaction",
+        "description": `${user} added the ${reaction.emoji.name} reaction to this [message](${reaction.message.url}) in ${reaction.message.channel}.`,
+        "color": 9442302,
+        "timestamp": Date.now(),
+        "footer": {
+            "text": `ID: ${user.id}`
+        },
+        "thumbnail": {
+            "url": reaction.emoji.url
+        },
+        "author": {
+            "name": user.tag,
+            "icon_url": user.avatarURL()
+        }
+    };
 
-    }
-})
+    bot.channels.cache.get(config.channelid).send({ embeds: [embed] });
+});
